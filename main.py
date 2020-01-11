a9d5b895eb333e515042345a60b5e9c0a2b0ab55
Mike <mike@parentsquare.com>
2020-01-10 16:37:51 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+import os
+from threading import Thread
+from time import sleep
+
+SCREEN_MANAGER = ScreenManager()
+
+
+class ProjectNameGUI(App):
+    """
+    Class to handle running the GUI Application
+    """
+
+    def build(self):
+        """
+        Build the application
+        :return: Kivy Screen Manager instance
+        """
+        return SCREEN_MANAGER
+
+
+Window.clearcolor = (1, 1, 1, 1)  # White
+
+
+class MainScreen(Screen):
+
+    def getStarted(self):
+        SCREEN_MANAGER.current = 'image_screen'
+
+
+class OneScreen(Screen):
+    def __init__(self, **kwargs):
+            Builder.load_file('One.kv')
+            super(OneScreen, self).__init__(**kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Builder.load_file('main.kv')
+SCREEN_MANAGER.add_widget(MainScreen(name='main'))
+SCREEN_MANAGER.add_widget(OneScreen(name='One'))
+
