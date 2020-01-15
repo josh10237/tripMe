9ef7102e32a3f4041658f61ab585832ab7cb3bcc
Josh Benson <josh.benson.805@gmail.com>
2020-01-15 00:28:22 -0800

    Finished Search screen with working API

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ email = ""
 name = ""
 age = ""
 gender = ""
+len = 0
 windowcolor = (0, .6, .6, 1)
 personalData = ["name", "email", "age", "gender"]
+
 
 class TripMe(App):
     def build(self):
@@ -29,7 +31,9 @@ class TripMe(App):
 
 
 Window.clearcolor = windowcolor
-#main color: (3, 1, 1, 1)
+
+
+# main color: (3, 1, 1, 1)
 
 
 class MainScreen(Screen):
@@ -37,10 +41,11 @@ class MainScreen(Screen):
     def getStarted(self):
         SCREEN_MANAGER.current = 'one'
 
+
 class one(Screen):
     def __init__(self, **kwargs):
-            Builder.load_file('one.kv')
-            super(one, self).__init__(**kwargs)
+        Builder.load_file('one.kv')
+        super(one, self).__init__(**kwargs)
 
     def whyDoWeAsk(self):
         SCREEN_MANAGER.current = 'why'
@@ -61,8 +66,6 @@ class one(Screen):
         else:
             self.ids.info_error.text = check
 
-
-
     def checkInfo(self, name, email, age, gender):
         # if (name.find(" ") == -1):
         #     return "Name must have first and last seperated by space"
@@ -81,13 +84,14 @@ class one(Screen):
 
 class why(Screen):
     def __init__(self, **kwargs):
-            Builder.load_file('why.kv')
-            super(why, self).__init__(**kwargs)
+        Builder.load_file('why.kv')
+        super(why, self).__init__(**kwargs)
+
 
 class two(Screen):
     def __init__(self, **kwargs):
-            Builder.load_file('two.kv')
-            super(two, self).__init__(**kwargs)
+        Builder.load_file('two.kv')
+        super(two, self).__init__(**kwargs)
 
     def search(self):
         SCREEN_MANAGER.current = 'search'
@@ -95,13 +99,35 @@ class two(Screen):
 
 class search(Screen):
     def __init__(self, **kwargs):
-            Builder.load_file('search.kv')
-            super(search, self).__init__(**kwargs)
+        Builder.load_file('search.kv')
+        super(search, self).__init__(**kwargs)
 
     def searchNow(self):
         location = self.ids.location_search.text
-        self.apiCall(location)
+        result = self.apiCall(location)
+        tmpList = list()
+        counter = 0
+        for entry in (result["data"]):
+            tmpList.append(entry["result_object"]["location_string"])
+            counter += 1
 
+        fill = 10 - counter
+        for i in range(fill):
+            tmpList.append("")
+
+        self.ids.sResult1.text = tmpList[0]
+        self.ids.sResult2.text = tmpList[1]
+        self.ids.sResult3.text = tmpList[2]
+        self.ids.sResult4.text = tmpList[3]
+        self.ids.sResult5.text = tmpList[4]
+        self.ids.sResult6.text = tmpList[5]
+        self.ids.sResult7.text = tmpList[6]
+        self.ids.sResult8.text = tmpList[7]
+        self.ids.sResult9.text = tmpList[8]
+        self.ids.sResult10.text = tmpList[9]
+
+        if (fill == 10):
+            self.ids.sResult1.text = "No Search Results"
 
     def apiCall(self, location):
         url = "https://tripadvisor1.p.rapidapi.com/locations/search"
@@ -110,10 +136,14 @@ class search(Screen):
 
         headers = {
             'x-rapidapi-host': "tripadvisor1.p.rapidapi.com",
-            'x-rapidapi-key': "b8604d934emsh388e7aff14f4d7ep1eb9efjsn80019256d442"
+            'x-rapidapi-key': "7a0f0aff90msh861007d6addde50p1016a9jsn76f3d84a1cd0"
         }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        print(response.text)
+        response = requests.get(url, headers=headers, params=querystring)
+        result = response.json()
+        return result
+
+
+
 
 
 Builder.load_file('main.kv')
